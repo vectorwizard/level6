f2d395886ef881626f888e4f574d7362a8a7d169
Arijit <arijitroylochan@gmail.com>
2026-08-25 00:19:06 +0530

    FIrst Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.get("/health", (req,res)=>{
+    return res.status(200).json({message: "All good"})
+})
+
+app.get("/", (req,res)=>{
+    return res.status(200).json({message: "Hello from Arijit"})
+})
+
+app.listen(port, ()=>{
+    console.log(`Server started ${port}`)
+})
