d77debae213fce241049fd058bfdc7a7556377dc
Arijit <arijitroylochan@gmail.com>
2026-08-25 01:12:30 +0530

    second commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get("/health", (req,res)=>{
 })
 
 app.get("/", (req,res)=>{
-    return res.status(200).json({message: "Hello from Arijit"})
+    return res.status(200).json({message: "Hello from Arijit v2"})
 })
 
 app.listen(port, ()=>{
